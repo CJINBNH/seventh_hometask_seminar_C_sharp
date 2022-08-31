4cfde2832bd9b85c12ca807997b00fecd4800dd0
CJINBNH <alex_egorov_89@mail.ru>
2022-08-31 20:42:39 +0400

    Решил задачу, но не понял как ограничить вещественное число до десятых

--- task_047/Program.cs
+++ task_047/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+Random ran = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = ran.NextDouble();
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
